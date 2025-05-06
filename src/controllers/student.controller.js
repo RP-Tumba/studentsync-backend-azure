4dd45732193b04315353d4d3b0c62887c88e58e4
UWAJENEZA Noella Fabiola <uwajenoellafa22@gmail.com>
2025-05-06 21:19:32 +0200

    feat:insert_new_student_already_done

--- src/controllers/student.controller.js
+++ src/controllers/student.controller.js
@@ -7,6 +7,14 @@
 import pool from "../config/db.js";
 import { logger } from "../utils/index.js";
 
+import { Client } from "pg";
+import express from "express";
+const stdInfo = express();
+stdInfo.use(express.json())
+
+
+
+
 export const getAllStudents = async (req, res) => {
   try {
     const students = await pool.query("SELECT * FROM students");
@@ -23,3 +31,23 @@ export const getAllStudents = async (req, res) => {
     });
   }
 };
+
+
+export const insertStudent = async (req,res)=>{
+  try{
+    const putData = await pool.query("insert into students values(3,'Noellaa','Uwajeneza','S1221112','noella@gmail.com','2002-12-31T22:01:00.000Z','0792435990','2022-12-31T22:00:00.000Z','words.jpg','2025-06-06T12:40:38.436Z','2025-05-06T12:40:38.436Z')")
+    res.status(200).json({
+      success: true,
+      count: students.rows.length,
+      data: students.rows,
+    });
+  } catch (err) {
+    logger.error(err.message);
+    res.status(500).json({
+      success: false,
+      message: `An unexpected error occurred in GET/STUDENTS, ${err?.message}`,
+    });
+  }
+}
+
+
