341081d9f6cdd1c6806bec0b034495bff29bc9fc
Elie-Dev250 <iradukundaelie71@gmail.com>
2025-05-07 21:14:52 +0200

    update

--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -5,11 +5,14 @@
  * Add more routes for creating, updating, and deleting students as needed.
  */
 import express from "express";
-import { getAllStudents, updateStudentById } from "../controllers/student.controller.js";
+import { deletingbyid, getAllStudents, insertingstudent,updateStudentById, selectingstudentbyid,  updatestudents } from "../controllers/student.controller.js";
 
 const router = express.Router();
 
 router.get("/", getAllStudents);
 router.put('/:id',updateStudentById);
-
+router.delete('/:id',deletingbyid);
+router.get('/:id',selectingstudentbyid);
+router.put('/:id',updatestudents)
+router.post('/',insertingstudent)
 export default router;
